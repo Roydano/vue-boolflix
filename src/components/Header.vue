1581434e5314bd17872fb7f22b696f7895f2afb7
Roydano <roy.dano90@gmail.com>
2021-07-12 15:00:32 +0200

    impostazione di come vogliamo la struttura

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search_bar">
+      <h1>Boolflix</h1>
+      <form action="">
+          <input type="text">
+          <button>Cerca</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+      <div class="card_movie">
+          titolo, titolo-originale, lingua, voto
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+
+}
+</script>
+
+<style>
+
+</style>
